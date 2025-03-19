42d54dc6c5ad7faa9e487af42ca490340065f5d4
azzkiabdl <147375620+azzkiabdl@users.noreply.github.com>
2025-03-19 08:12:57 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+using System;
+class PemrosesData
+{
+    public T DapatkanNilaiTerbesar<T>(T data1, T data2, T data3)
+    {
+        dynamic nilai1 = (dynamic)data1;
+        dynamic nilai2 = (dynamic)data2;
+        dynamic nilai3 = (dynamic)data3;
+
+        dynamic nilaiMax = nilai1;
+        if (nilai2 > nilaiMax)
+        {
+            nilaiMax = nilai2;
+        }
+        if (nilai3 > nilaiMax)
+        {
+            nilaiMax = nilai3;
+        }
+        return nilaiMax;
+    }
+
+    private static void Main(string[] Args)
+    {
+        PemrosesData pemroses = new PemrosesData();
+        float terbesar = pemroses.DapatkanNilaiTerbesar(1, 2, 3);
+        Console.WriteLine(terbesar);
+    }
+}
